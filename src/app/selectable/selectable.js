338276899a7cecf9fb1f71291176b70b906e3228
Jenish Rabadiya <jenishrabadiya@outlook.com>
2018-02-23 10:36:57 +0530

    set tabular format

--- src/app/selectable/selectable.js
+++ src/app/selectable/selectable.js
@@ -3,18 +3,16 @@ define(['ko'], function(ko){
         viewModel: function(params){
             var templateRef = ko.isObservable(params.templateRef) ? params.templateRef : ko.observable(params.templateRef);
             var repeator = ko.isObservable(params.repeator) ? params.repeator : ko.observableArray(params.repeator);
+            var element = params.element;
 
-            var initialize = function(element){
-                $(element).bind("mousedown", function (e) {
-                    e.metaKey = false; //To prevent discontigous selection
-                }).selectable({
-                    filter: ".selectable-item"
-                });
-            };
+            $(element.parentElement).bind("mousedown", function (e) {
+                e.metaKey = false; //To prevent discontigous selection
+            }).selectable({
+                filter: "td"
+            });
 
             return {
                 templateRef: templateRef,
-                initialize: initialize,
                 repeator: repeator
             }
         },
