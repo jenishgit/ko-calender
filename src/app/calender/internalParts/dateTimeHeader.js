29969539a95a238d2c2d507dacfcb4dcf54f97bb
Jenish Rabadiya <jenishrabadiya@outlook.com>
2018-02-27 14:00:49 +0530

    configuration refactored.

--- src/app/calender/internalParts/dateTimeHeader.js
+++ src/app/calender/internalParts/dateTimeHeader.js
@@ -1,9 +1,9 @@
-define(['ko', 'app/calender/config/templateConfig'], function(ko, templateConfig){
+define(['ko'], function(ko){
     ko.components.register('ko-dateTimeHeader',{
         viewModel: function(params){
             var templateRef = ko.isObservable(params.templateRef) ? params.templateRef : ko.observable(params.templateRef);
             var repeator = ko.isObservable(params.repeator) ? params.repeator : ko.observableArray(params.repeator);
-
+            var templateConfig = params.templateConfig;
             return {
                 templateRef: templateRef,
                 repeator: repeator,
--- src/app/selectable/selectable.js
+++ src/app/selectable/selectable.js
@@ -1,10 +1,11 @@
-define(['ko', 'app/calender/config/templateConfig'], function(ko, templateConfig){
+define(['ko'], function(ko){
     ko.components.register('ko-selectable',{
         viewModel: function(params){
             var templateRef = ko.isObservable(params.templateRef) ? params.templateRef : ko.observable(params.templateRef);
             var repeator = ko.isObservable(params.repeator) ? params.repeator : ko.observableArray(params.repeator);
             var element = params.element;
             var data = ko.isObservable(params.data) ? params.data : ko.observable(params.data);
+            var templateConfig = params.templateConfig;
 
             $(element.parentElement).bind("mousedown", function (e) {
                 e.metaKey = false; //To prevent discontigous selection
