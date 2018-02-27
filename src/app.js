05e2fb33397b29fba24bab25340ecbbc94b2adbe
Jenish Rabadiya <jenishrabadiya@outlook.com>
2018-02-27 17:21:20 +0530

    jquery touch punch integrated

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ requirejs.config({
         ko: '../node_modules/knockout/build/output/knockout-latest',
         bootstrap: '../node_modules/bootstrap/dist/js/bootstrap.min',
         jqueryUI: '../node_modules/jquery-ui-dist/jquery-ui.min',
+        uiPunch: '../node_modules/jquery-ui-touch-punch/jquery.ui.touch-punch.min',
         text: '../node_modules/text/text'
     },
     map: {
@@ -29,4 +30,6 @@ requirejs(
         'text!../node_modules/bootstrap/dist/css/bootstrap.min.css',
         'jqueryUI', 
         'text!../node_modules/jquery-ui-dist/jquery-ui.min.css'
-    ])
+    ], function(){
+        requirejs(['uiPunch'])
+    })
