77b02bf4196b8493362627b05611aacd3dffb154
Jenish Rabadiya <jenishrabadiya@outlook.com>
2018-03-14 15:47:52 +0530

    ploting done

--- src/app/selectable/selectable.js
+++ src/app/selectable/selectable.js
@@ -14,23 +14,54 @@ define(['ko'], function(ko){
                 selected: function(event, ui) {
                 },
                 stop: function(event, ui){
-                    var selectedItems = $('.ui-selected', this);
-                    var firstSelectedCellContext = ko.contextFor(selectedItems[0]).$data;
-                    var lastSelectedCellContext = ko.contextFor(selectedItems[selectedItems.length-1]).$data;
-                    firstSelectedCellContext.end = lastSelectedCellContext.end;
-                    var colSpan = 0;
-                    for(var i = selectedItems.length - 1; i > 0; i--){
-                        colSpan = colSpan + selectedItems[i].colSpan;
-                        $(selectedItems[i]).remove();
-                    }
-                    selectedItems[0].colSpan  += colSpan;
-                    var t = ko.contextFor(selectedItems[0]);
-                    selectedItems.removeClass('ui-selected');
+                    onSelectionDone();
                 }
             });
 
+            var onSelectionDone = function(){
+                var selectedItems = $('.ui-selected', element);
+                var firstSelectedCellContext = ko.contextFor(selectedItems[0]).$data;
+                var lastSelectedCellContext = ko.contextFor(selectedItems[selectedItems.length-1]).$data;
+                firstSelectedCellContext.end = lastSelectedCellContext.end;
+                var colSpan = 0;
+                for(var i = selectedItems.length - 1; i > 0; i--){
+                    colSpan = colSpan + selectedItems[i].colSpan;
+                    $(selectedItems[i]).remove();
+                }
+                selectedItems[0].colSpan  += colSpan;
+                selectedItems.removeClass('ui-selected');
+                selectedItems.addClass(calenderConfig.cssClass.occupied);
+            }
+
+            var plotData = function(data, startDate, endDate){
+                data().placements().forEach(item => {
+                    if (startDate < item.startDate() && item.endDate() < endDate) {
+                        setTimeout(function(){
+                            var itemToBeSelected = [];
+                            var tds = $(element).children('td');
+                            for (let index = 1; index < tds.length; index++) {
+                                const td = tds[index];
+                                var context = ko.contextFor(td).$data;
+                                if(context.start.getTime() == item.startDate().getTime()){
+                                    for (let i = index; i < tds.length ; i++){
+                                        const temp = tds[i];
+                                        itemToBeSelected.push(temp);
+                                        var context2 = ko.contextFor(temp).$data;
+                                        if ( context2.end.getTime() == item.endDate().getTime() ){
+                                            break;
+                                        }
+                                    }
+                                    break;
+                                }
+                            }
+                            $(itemToBeSelected).addClass('ui-selected');
+                            onSelectionDone();
+                        }, 1000);
+                    }
+                });
+            }
+            plotData(data, calenderConfig.startDate(), calenderConfig.endDate());
             var instance = $(element).selectable("instance");
-            debugger;
 
             return {
                 templateRef: templateRef,
